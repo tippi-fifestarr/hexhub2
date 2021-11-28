c044237a34cfb5346448c212cc3052bbfe87e83b
tippi-fifestarr <62179036+tippi-fifestarr@users.noreply.github.com>
2021-11-27 21:38:46 -0600

    test 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Hexagon, TiledHexagons } from "tiled-hexagons";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <TiledHexagons
+            maxHorizontal={1}
+            tileSideLengths={120}
+            tileTextStyles={{
+              fontFamily: "Font Awesome Brands",
+              fontSize: "58px",
+              fill: "white",
+            }}
+            tileGap={0}
+            tileBorderRadii={9}
+            tiles={[
+              {
+                text: "step.0",
+                fill: "#4267b2",
+                shadow: "#1a2947",
+                href: "https://tippi-fifestarr.github.io/ChainlinkTeamFun/",
+              },
+              {
+                text: "step.1",
+                fill: "#282828",
+                shadow: "#1a1a1a",
+                href: "https://github.com/josephsurin/tiled-hexagons",
+              },
+              {
+                text: "step.2",
+                fill: "#1dcaff",
+                shadow: "#0084b4",
+                href: "https://tippi-fifestarr.github.io/ethereum-boilerplate/",
+              },
+              {
+                text: "step.3",
+                fill: "#ED01ED",
+                shadow: "#0084b4",
+                href: "http://localhost:48084/ipns/chat.thedisco.zone/",
+              },
+            ]}
+          />
+          <Hexagon
+            sideLength={200}
+            text="Start Here"
+            textStyle={{ fill: "blue" }}
+            href="https://github.com/tippi-fifestarr/hexhub2"
+          />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
